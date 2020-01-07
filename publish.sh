fc224c3f5c7e68211a2ff507698383771740e782
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-07 17:33:15 +0000

    creating index etc

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@ rm index.zip
 cd package
 zip -X -r "../index.zip" *
 cd ..
-aws lambda update-function-code --function-name CreateThumbnailFromImage-dev --zip-file fileb://index.zip
+aws lambda update-function-code --function-name CompressAvatarCreatePictureObject-dev --zip-file fileb://index.zip
